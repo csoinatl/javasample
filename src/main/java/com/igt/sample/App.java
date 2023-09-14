c4854cb4744f542c762ad5e3e89c9ef3c2d43bdf
Charles So <charlie.so@gmail.com>
2023-09-14 16:28:35 -0400

    Initial Commit and Push

---
+++ src/main/java/com/igt/sample/App.java
@@ -0,0 +1,35 @@
+package com.igt.sample;
+
+import java.util.Objects;
+
+/**
+ * Write a method to replace all spaces in a string with '%20' in language of your choice,
+ * verify your code by adding at least 5 test cases (suppliers to verify)
+ */
+public class App {
+
+    private static final String REPLACEMENT_STRING = "%20";
+
+    public static String replaceSpaceUsingReplaceAll(String stringToReplace) {
+        if (Objects.isNull(stringToReplace))
+            return "";
+
+        return stringToReplace.replaceAll("\\s", REPLACEMENT_STRING);
+    }
+
+    public static String replaceSpaceUsingCharArray(String stringToReplace) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (Objects.isNull(stringToReplace))
+            return "";
+
+        for (char curChar : stringToReplace.toCharArray()) {
+            if (curChar == ' ') {
+                stringBuilder.append(REPLACEMENT_STRING);
+            } else {
+                stringBuilder.append(curChar);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+}
---
+++ src/test/java/com/igt/sample/AppTest.java
@@ -0,0 +1,35 @@
+package com.igt.sample;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    private static Stream<Arguments> getTestArguments() {
+        return Stream.of(
+                Arguments.of(null, ""),
+                Arguments.of("", ""),
+                Arguments.of(" ", "%20"),
+                Arguments.of("a b c", "a%20b%20c"),
+                Arguments.of("%20", "%20"),
+                Arguments.of("This is a test string.", "This%20is%20a%20test%20string.")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getTestArguments")
+    void testReplaceSpaceUsingReplaceAll(String input, String expected){
+        assertEquals(expected, App.replaceSpaceUsingReplaceAll(input));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getTestArguments")
+    void testReplaceUsingCharArray(String input, String expected){
+        assertEquals(expected, App.replaceSpaceUsingCharArray(input));
+    }
+}
